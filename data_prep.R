4a81cb0288c833d215291bc278dbf44cd2ef9be1
d.kotsila <d.kotsila@intelen.com>
2018-09-18 10:09:51 +0300

    import data

--- data_prep.R
+++ data_prep.R
@@ -1,1 +1,21 @@
-# Test script
+#####################################################
+# Data preparation for SwifKey project
+#
+# Dimitra 18/09/2018
+#####################################################
+library(data.table)
+library(tm)
+
+# Import data
+
+temp <- tempfile()
+download.file("https://d396qusza40orc.cloudfront.net/dsscapstone/dataset/Coursera-SwiftKey.zip", temp)
+
+# Unzip folder
+unzip (zipfile = temp)
+rm(temp)
+
+# Read all files
+list_of_files <- list.files(pattern = ".txt", recursive = TRUE)
+datalist = lapply(list_of_files, FUN=readLines,stringsFactors = F ) 
+t1 <- readLines(list_of_files[[1]])
